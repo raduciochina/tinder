05bc14bc35339964c8858f32fa8abc604a13f749
Radu Ciochina <ciochina7@gmail.com>
2021-10-22 18:27:30 +0300

    Commit tema 2 done.

--- app/src/main/java/com/example/tinder/LoginActivity.java
+++ app/src/main/java/com/example/tinder/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.tinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent i = getIntent();
+
+        String textDataEmail = i.getExtras().getString("textDataEmail", "");
+        String textDataPassword = i.getExtras().getString("textDataPassword", "");
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+
+        editTextEmail.setText(textDataEmail);
+        editTextPassword.setText(textDataPassword);
+
     }
 }
--- app/src/main/java/com/example/tinder/RegisterActivity.java
+++ app/src/main/java/com/example/tinder/RegisterActivity.java
@@ -2,13 +2,98 @@ package com.example.tinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btnRegister;
+    private EditText editTextNume;
+    private EditText editTextParola;
+    private EditText editTextEmail;
+    private EditText editTextTelefon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister = findViewById(R.id.register);
+        editTextEmail = findViewById(R.id.email);
+        editTextNume = findViewById(R.id.name);
+        editTextParola = findViewById(R.id.password);
+        editTextTelefon = findViewById(R.id.phone);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isValid()){
+                User user = new User();
+                user.setName(editTextNume.getText().toString());
+                user.setEmail(editTextEmail.getText().toString());
+                user.setPassword(editTextParola.getText().toString());
+                user.setPhone(editTextTelefon.getText().toString());
+
+                //Toast.makeText(RegisterActivity.this, user.toString(), Toast.LENGTH_SHORT).show();
+                }
+
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+
+                String textDataEmail = editTextEmail.getText().toString();
+                String textDataPassword = editTextParola.getText().toString();
+
+                i.putExtra("textDataEmail", textDataEmail);
+                i.putExtra("textDataPassword", textDataPassword);
+
+                startActivity(i);
+
+            }
+        });
     }
+
+    boolean isEmailValid(CharSequence email){
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    boolean isValidPassword(final String password) {
+
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
+    }
+    private boolean isValid(){
+        if(editTextNume.getText().toString().isEmpty()){
+            Toast.makeText(RegisterActivity.this, "Please enter your name!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(editTextTelefon.getText().toString().isEmpty()){
+            Toast.makeText(RegisterActivity.this, "Please enter your phone number!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!(isEmailValid(editTextEmail.getText().toString()))){
+            Toast.makeText(RegisterActivity.this, "Please enter your email!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(editTextParola.getText().toString().length()<8 &&!isValidPassword(editTextParola.getText().toString())){
+            Toast.makeText(RegisterActivity.this, "Please enter a more complex password!",Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
 }
